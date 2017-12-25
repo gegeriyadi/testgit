2ecd88c5b50a6c9f36f26f4f45c16c55bd9b44d4
Teguh Riyadi <gegeriyadi81@gmail.com>
2017-12-25 21:48:39 +0700

    fix github

--- github.php
+++ github.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec( 'cd /var/www/testgit/ && git reset --hard HEAD && git pull' );
+if ( $_POST['payload'] ) {
+  shell_exec( "./deploy.sh" );
 }
 ?>hi
